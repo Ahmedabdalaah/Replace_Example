5f0218efab75ffa1f955544605e3eb83c5115a76
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 02:54:32 +0200

    Add project files.

---
+++ Replace_Example/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Replace_Example;
+RepChar rep = new RepChar();
+Console.WriteLine("Please Enter input data ");
+string input = Console.ReadLine();
+Console.WriteLine("Enter char that you want cahnge");
+string ol = Console.ReadLine();
+Console.WriteLine("Enter new cahr you want replace ");
+string ne = Console.ReadLine();
+if((ol.Length==1) && (ne.Length == 1))
+{
+    try
+    {
+        char old = Convert.ToChar(ol);
+        char neww = Convert.ToChar(ne);
+        string output = rep.replace(input, old, neww);
+        Console.WriteLine("output : " + output);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+else if ((ol.Length != 1) || (ne.Length != 1))
+{
+    Console.WriteLine("Invalid Data");
+}
+
+
---
+++ Replace_Example/RepChar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace_Example
+{
+    internal class RepChar
+    {
+        public string replace(string original, char old, char neww)
+        {
+            string output = original.Replace(old, neww);
+            return output;
+        }
+    }
+}
